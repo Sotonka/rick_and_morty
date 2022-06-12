5af2c28e652dc956d9e4b9cb24f7d980dc2f8adb
Evgeny Alekseenko <punconge@gmail.com>
2022-06-12 16:03:28 +1000

    noUI

--- lib/feature/presentation/pages/person_screen.dart
+++ lib/feature/presentation/pages/person_screen.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/persons_list_wiget.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Characters'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+            color: Colors.white,
+          ),
+        ],
+      ),
+      body: PersonsList(),
+    );
   }
 }
