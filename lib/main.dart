056870ff87a51a2664baa0e0a790810435a47339
Evgeny Alekseenko <punconge@gmail.com>
2022-06-12 15:34:29 +1000

    dependency injecion

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty/feature/presentation/bloc/person_list_cubit/person_list_cubit.dart';
+import 'package:rick_and_morty/locator_service.dart' as di;
 
-void main() {
+import 'feature/presentation/bloc/search_bloc/search_bloc.dart';
+import 'feature/presentation/pages/person_screen.dart';
+import 'locator_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   runApp(const MyApp());
 }
 
@@ -9,8 +18,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(),
-    );
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider<PersonListCubit>(
+              create: (context) => sl<PersonListCubit>()..loadPerson()),
+          BlocProvider<PersonSearchBloc>(
+              create: (context) => sl<PersonSearchBloc>()),
+        ],
+        child: MaterialApp(
+          theme: ThemeData.dark().copyWith(
+            backgroundColor: Colors.black,
+            scaffoldBackgroundColor: Colors.grey,
+          ),
+          home: HomePage(),
+        ));
   }
 }
