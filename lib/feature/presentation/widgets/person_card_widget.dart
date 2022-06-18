a5a11ad23f122d84fd13d93c61a62c6d1ccf9a1f
Evgeny Alekseenko <punconge@gmail.com>
2022-06-18 11:40:29 +1000

    detailed information

--- lib/feature/presentation/widgets/person_card_widget.dart
+++ lib/feature/presentation/widgets/person_card_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rick_and_morty/common/app_colors.dart';
 import 'package:rick_and_morty/feature/domain/entities/person_entity.dart';
+import 'package:rick_and_morty/feature/presentation/pages/person_detail_screen.dart';
 import 'package:rick_and_morty/feature/presentation/widgets/person_cache_image_widget.dart';
 
 class PersonCard extends StatelessWidget {
@@ -9,105 +10,114 @@ class PersonCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: AppColors.cellBackground,
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      child: Row(
-        children: [
-          Container(
-            child: PersonCacheImage(
-                height: 166, width: 166, imageUrl: person.image),
-          ),
-          const SizedBox(
-            width: 16.0,
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 12.0,
-                ),
-                Text(
-                  person.name,
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                const SizedBox(
-                  height: 4.0,
-                ),
-                Row(
-                  children: [
-                    Container(
-                      height: 8.0,
-                      width: 8.0,
-                      decoration: BoxDecoration(
-                        color: person.status == 'Alive'
-                            ? Colors.green
-                            : Colors.red,
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 8.0,
-                    ),
-                    Expanded(
-                      child: Text(
-                        '${person.status} - ${person.species}',
-                        style: TextStyle(color: Colors.white),
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 12.0,
-                ),
-                Text(
-                  'Last known location:',
-                  style: TextStyle(color: AppColors.greyBackground),
-                ),
-                const SizedBox(
-                  height: 4.0,
-                ),
-                Text(
-                  person.location.name,
-                  style: TextStyle(color: Colors.white),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                const SizedBox(
-                  height: 12.0,
-                ),
-                Text(
-                  'Origin',
-                  style: TextStyle(color: AppColors.greyBackground),
-                ),
-                const SizedBox(
-                  height: 4.0,
-                ),
-                Text(
-                  person.origin.name,
-                  style: TextStyle(color: Colors.white),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                const SizedBox(
-                  height: 16.0,
-                ),
-              ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => PersonDetailPage(person: person),
+            ));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: AppColors.cellBackground,
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Row(
+          children: [
+            Container(
+              child: PersonCacheImage(
+                  height: 166, width: 166, imageUrl: person.image),
             ),
-          ),
-          const SizedBox(
-            width: 12.0,
-          ),
-        ],
+            const SizedBox(
+              width: 16.0,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 12.0,
+                  ),
+                  Text(
+                    person.name,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 4.0,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        height: 8.0,
+                        width: 8.0,
+                        decoration: BoxDecoration(
+                          color: person.status == 'Alive'
+                              ? Colors.green
+                              : Colors.red,
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 8.0,
+                      ),
+                      Expanded(
+                        child: Text(
+                          '${person.status} - ${person.species}',
+                          style: TextStyle(color: Colors.white),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 12.0,
+                  ),
+                  const Text(
+                    'Last known location:',
+                    style: TextStyle(color: AppColors.greyBackground),
+                  ),
+                  const SizedBox(
+                    height: 4.0,
+                  ),
+                  Text(
+                    person.location.name,
+                    style: const TextStyle(color: Colors.white),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(
+                    height: 12.0,
+                  ),
+                  const Text(
+                    'Origin',
+                    style: TextStyle(color: AppColors.greyBackground),
+                  ),
+                  const SizedBox(
+                    height: 4.0,
+                  ),
+                  Text(
+                    person.origin.name,
+                    style: TextStyle(color: Colors.white),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              width: 12.0,
+            ),
+          ],
+        ),
       ),
     );
   }
